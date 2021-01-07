b5c0490368f4981617b321b5e479913e8f27099c
Greg Samuelson <grssam@gmail.com>
2021-01-07 11:41:48 -0800

    merge fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
@@ -6,10 +6,42 @@ import Navbar from './components/Navbar'
 import Form from './components/Form'
 import Blog from './components/Blog'
 import background from './img/background.jpg'
+import PostAPI from '../src/utils/PostAPI/PostAPI'
+
+const {
+  getPosts,
+  createPost,
+} = PostAPI
 
 const App = () => {
 
+  const [postState, setPostState] = useState({
+    post: '',
+    posts: []
+  })
 
+  postState.handleInputChange = event => {
+    setPostState({ ...postState, [event.target.name]: event.target.value })
+  }
+
+  postState.handleAddPost = event => {
+    let posts = JSON.parse(JSON.stringify(postState.posts))
+    createPost({
+      text: postState.post
+    })
+      .then(({ data: post }) => {
+        posts.push(post)
+        setPostState({ ...postState, posts, post: '' })
+      })
+      .catch(err => console.error(err))
+  }
+
+  useEffect(() => {
+    getPosts()
+      .then(({ data: posts }) => {
+        setPostState({ ...postState, posts })
+      })
+  }, [])
 
   const [drawerState, setDrawerState] = useState({
     left: false
